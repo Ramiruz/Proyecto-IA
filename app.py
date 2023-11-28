93d8aed18ba205d2933d0aeb754947cb98328b15
Carlos Alberto Salcedo Rodriguez <csalcedo@utb.edu.co>
2023-11-27 21:04:16 -0500

    arreglando errores de subida de archivos

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, url_for, render_template, request
 from Proyecto_IA.ACSIA import transcribir_audio, generar_respuesta 
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
@@ -11,8 +12,13 @@ def assembly():  # put application's code here
 
 @app.post('/')
 def pene():
-   audio = request.form['audio']   
-   texto_transcrit = transcribir_audio("46f3fc0a6e364888acf89978f15a5d24", audio)
+   audio = request.form['audio']
+
+   audio.save(os.path.join(app.config['UPLOAD_FOLDER'], audio.filename))
+   ruta_archivo = os.path.abspath(os.path.join(app.config['UPLOAD_FOLDER'], audio.filename))
+
+
+   texto_transcrit = transcribir_audio("46f3fc0a6e364888acf89978f15a5d24", ruta_archivo)
    modelo_gpt2 = GPT2LMHeadModel.from_pretrained('gpt2')
    tokenizer_gpt2 = GPT2Tokenizer.from_pretrained('gpt2')
    pregunta = "\nTell me about the content of the transcribed text. Who is the patrick?"
