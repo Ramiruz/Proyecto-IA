856833af32a02551ff453070b83de5b1c0e248b2
Ramiruz <andresjuanramireztorres99@gmail.com>
2023-11-29 00:23:57 -0500

    Audios y comentarios UI

--- Proyecto-IA-flask/app.py
+++ Proyecto-IA-flask/app.py
@@ -1,50 +1,70 @@
+# Importar las bibliotecas necesarias
 from flask import Flask, url_for, render_template, request
 import assemblyai as aai
 from summarizer import Summarizer
 from werkzeug.utils import secure_filename
 import os
 
+# Configurar la aplicación Flask
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './uploads'
 app.config['ALLOWED_EXTENSIONS'] = {'mp3'}
 
+# Función para realizar la transcripcion de audio utilizando AssemblyAI
+def transcribe_audio(api_key, audio_path):
 
-def transcribir_audio(api_key, audio_path):
-    aai.settings.api_key = api_key
-    transcriber = aai.Transcriber()
-    transcript = transcriber.transcribe(audio_path)
-    oraciones = transcript.get_paragraphs()
-    oraciones_texto = [parrafo.text for parrafo in oraciones]
-    texto_transcrito = ' '.join(oraciones_texto)
-    return texto_transcrito
+    aai.settings.api_key = api_key      # Establecer la clave API de AssemblyAI
+    
+    transcriber = aai.Transcriber()     # Inicializar el transcriptor de AssemblyAI
+    
+    transcript = transcriber.transcribe(audio_path)     # Transcribir el archivo de audio
+    
+    # Se obtiene el texto del audio por partes y se unen en un solo texto transcrito
+    paragraphs = transcript.get_paragraphs()
+    text_sentences = [paragraph.text for paragraph in paragraphs]
+    transcribed_text = ' '.join(text_sentences)
+    
+    return transcribed_text
 
+
+# Ruta para el método GET, que renderiza la página principal
 @app.get('/')
-def assembly():  # put application's code here
+def assembly():
     css_url = url_for('static', filename='style.css')
     return render_template("index.html", css_url=css_url)
 
+
+# Ruta para el método POST, que procesa la carga y transcripción del archivo de audio
 @app.post('/')
 def index():
     if request.method == 'POST':
         api_key_assemblyai = "46f3fc0a6e364888acf89978f15a5d24"
 
-
+        # Obtener el archivo de audio desde la solicitud
         file = request.files['audio']
-        filename= secure_filename(file.filename)
+        filename = secure_filename(file.filename)
 
+        # Guardar el archivo de audio en la carpeta de carga
         audio_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(audio_path)
 
-        texto_transcrito = transcribir_audio(api_key_assemblyai, audio_path)
+        # Transcribir el audio utilizando AssemblyAI
+        transcribed_text = transcribe_audio(api_key_assemblyai, audio_path)
 
+        # Generar un resumen utilizando el modelo de resumen BERT
         bert_model = Summarizer()
-        bert_summary = ''.join(bert_model(texto_transcrito, min_length=60))
+        bert_summary = ''.join(bert_model(transcribed_text, min_length=60))
 
-        return render_template('index.html', transcript=texto_transcrito, summary=bert_summary)
+        # Renderizar la página con el texto transcrito y el resumen BERT
+        return render_template('index.html', transcript=transcribed_text, summary=bert_summary)
+
+    # Renderizar la página si no hay una solicitud POST
+    return render_template('index.html', transcript="", summary="")
 
 
-    return render_template('index.html', transcript=texto_transcrito, summary=bert_summary)
-
+# Ejecutar la aplicación si el script se ejecuta directamente
 if __name__ == "__main__":
+
+    # Crear la carpeta de carga si no existe
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     app.run(debug=True)
